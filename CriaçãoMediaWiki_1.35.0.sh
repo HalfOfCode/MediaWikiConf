420f771280236d87f9887478e9bf5136a4f07f4e
Luan Farias de Melo <46304677+LuanFMelo@users.noreply.github.com>
2020-11-20 22:11:13 -0300

    Update and rename CriaçãoMediaWiki_1.31.1.sh to CriaçãoMediaWiki_1.35.0.sh

--- CriaçãoMediaWiki_1.35.0.sh
+++ CriaçãoMediaWiki_1.35.0.sh
@@ -10,21 +10,21 @@ sudo apt install apache2 -y
 sudo apt install mysql-server -y
 
 #Installing PHP
-sudo apt install php-pear php-fpm php-dev php-zip php-curl php-xmlrpc php-gd php-mysql php-mbstring php-xml libapache2-mod-php -y
+sudo apt install php-pear php-fpm php-dev php-zip php-curl php-xmlrpc php-gd php-mysql php-mbstring php-xml libapache2-mod-php php-intl php-apcu -y
 
 
 #Baixando MediaWiki
 cd /var/www/html/
-sudo wget https://releases.wikimedia.org/mediawiki/1.31/mediawiki-1.31.1.tar.gz
+sudo wget https://releases.wikimedia.org/mediawiki/1.35.0/mediawiki-1.35.0.tar.gz
 
 #Extraindo Wiki
-sudo tar xvzf mediawiki-1.31.1.tar.gz
+sudo tar xvzf mediawiki-1.35.0.tar.gz
 
 #Movendo pasta
-mv mediawiki-1.31.1 wiki
+mv mediawiki-1.35.0 wiki
 
 #Excluindo pasta
-rm -rf mediawiki-1.31.1.tar.gz
+rm -rf mediawiki-1.35.0.tar.gz
 
 #Permissoes diretorios
 chmod 777 /var/www/
